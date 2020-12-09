f99994e171f21b7c73fe6ad51b9533d6fc80359a
Alex Nagy <therebeloper@gmail.com>
2020-12-09 12:13:54 +0200

    Initial Commit

---
+++ WKViewDemo/WKViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WKViewDemoApp.swift
+//  WKViewDemo
+//
+//  Created by Alex Nagy on 09.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct WKViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
