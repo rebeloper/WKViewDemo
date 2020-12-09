ae07f6254a64b879b09c45192ed3b5d3187302bc
Alex Nagy <therebeloper@gmail.com>
2020-12-09 13:14:49 +0200

    starter

--- WKViewDemo/ContentView.swift
+++ WKViewDemo/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isSheetPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(spacing: 12) {
+                Divider()
+                
+                // 1. Push a WebView with a url
+                
+                Button(action: {
+                    isSheetPresented.toggle()
+                }, label: {
+                    Text("Present WebView")
+                }).sheet(isPresented: $isSheetPresented, content: {
+                    NavigationView {
+                        // 2. Present WebView in a modal with hiding the back button
+                        
+                        // 3. Present a customized WebView in a modal
+                        
+                    }
+                })
+                Spacer()
+            }
+            .navigationBarTitle("WKView")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        
     }
 }
 
